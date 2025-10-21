47f2d71b203fb52035b1072c1f4f823b214e73a5
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-10-21 03:13:11 -0300

    Add generated cache and project files for FinShark
    - Created cache files for Razor views and static web assets.
    - Added project.assets.json and project.nuget.cache for dependency management.
    - Included NuGet specification files for project dependencies.
    - Updated project properties for .NET 9.0 compatibility.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. This is .NET's Dependency Injection container
+//(like Provider or GetIt in Flutter). You register services here that can be injected throughout
+//your app.This is .NET's Dependency Injection container (like Provider or GetIt in Flutter). 
+//You register services here that can be injected throughout your app.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//Checks if you're running in development mode (similar to kDebugMode in Flutter).
+//UseSwagger() & UseSwaggerUI(): Only in development,
+ //this enables the Swagger UI at /swagger where you can test your API endpoints interactively.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
